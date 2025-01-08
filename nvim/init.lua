657fa787e83ecf299be142623af227821916b513
Miguel Jover <78712025+mianjoto@users.noreply.github.com>
2025-01-08 18:49:46 -0500

    Add custom javascript-mocha snippets

--- nvim/init.lua
+++ nvim/init.lua
@@ -399,7 +399,7 @@ require('lazy').setup({
           {
             'rafamadriz/friendly-snippets',
             config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load { paths = './snippets' }
             end,
           },
         },
